f83c24113ecd9ad9591b28260c3ad58c87c8eec6
ToruNiina <niina.toru.68u@gmail.com>
2019-01-10 13:04:22 +0900

    fix: output commas, not whitespaces

--- src/afmize.cpp
+++ src/afmize.cpp
@@ -102,7 +102,7 @@ void write_csv(const stage<Real>& stg, const std::string& out)
     {
         for(std::size_t i=0; i<stg.x_pixel(); ++i)
         {
-            ofs << stg(i, j) << ' ';
+            ofs << stg(i, j) << ',';
         }
         ofs << '\n';
     }
