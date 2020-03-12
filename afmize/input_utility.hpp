5170b71d2311f616de9483716d852c2615ff6280
ToruNiina <niina.toru.68u@gmail.com>
2020-03-12 15:11:31 +0900

    :recycle: use toml::format_error to gen err msgs

--- afmize/input_utility.hpp
+++ afmize/input_utility.hpp
@@ -10,26 +10,35 @@ namespace afmize
 
 template<typename T>
 std::enable_if_t<std::is_floating_point<T>::value, T>
-read_as_angstrom(const toml::value& v, const std::string& name)
+read_as_angstrom(const toml::value& v)
 {
     using namespace std::literals::string_literals;
+
+    const std::vector<std::string> hints{ // for error message.
+            "\"1.0nm\"                : 1.0 nano meter.",
+            "\"1.0Å\" or \"1.0angstrom\": 1.0 angstrom.",
+            "\"1.0pm\"                : 1.0 pico meter.",
+            "1.0 (floating)         : 1.0 angstrom.",
+            "1   (integer)          : 1.0 angstrom."
+        };
 
     if(v.is_floating()) {return v.as_floating();}
     if(v.is_integer())  {return v.as_integer();}
     if(!v.is_string())
     {
-        throw std::runtime_error("`"s + name + "` has invalid type `"s +
-            toml::stringize(v.type()) + "` (none of string, floating, "
-            "or integer)."s);
+        throw std::runtime_error(toml::format_error("input has invalid type " +
+            toml::stringize(v.type()) + ", none of string, floating, or integer",
+            v, "here, only the following inputs are allowed", hints));
     }
 
     const std::string str = v.as_string();
-    toml::detail::location<std::string> loc(name, str);
+    toml::detail::location<std::string> loc("internal region", str);
     const auto result = toml::detail::parse_floating(loc);
     if(!result)
     {
         throw std::runtime_error(toml::format_error("[error] `"s + str +
-            "` is not a valid floating-unit pair."s, v, "here"));
+            "` is not a valid floating-unit pair."s,
+            v, "here, only the following inputs are allowed", hints));
     }
     const auto val = result.unwrap().first;
     const std::string unit(loc.iter(), loc.end());
@@ -58,7 +67,9 @@ read_as_angstrom(const toml::value& v, const std::string& name)
     }
 
     throw std::runtime_error(toml::format_error("[error] unknown length unit `"s
-        + unit + "` appeared."s, v, "here"));
+        + unit + "` appeared."s,
+        v, "here, only the following inputs are allowed", hints));
+
 }
 
 } // afmize
