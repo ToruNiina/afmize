0fbdd3b678d76afb133c96637103372fb5d49e73
ToruNiina <niina.toru.68u@gmail.com>
2020-10-23 16:26:01 +0900

    :recycle: make score funcs structs to use paras inside

--- include/afmize/score.hpp
+++ include/afmize/score.hpp
@@ -6,55 +6,60 @@
 namespace afmize
 {
 
-template<typename Real, typename Mask>
-Real score_cosine_similarity(
-        const stage<Real>& lhs, const stage<Real>& rhs, const Mask& mask)
+struct score_cosine_similarity_t
 {
-    Real numer  = 0;
-    Real denom1 = 0;
-    Real denom2 = 0;
-
-    for(std::size_t y=mask.lower_bounding_y(); y <= mask.upper_bounding_y(); ++y)
+    template<typename Real, typename Mask>
+    Real operator()(const stage<Real>& lhs, const stage<Real>& rhs, const Mask& mask) const
     {
-        for(std::size_t x=mask.lower_bounding_x(); x <= mask.upper_bounding_x(); ++x)
+        Real numer  = 0;
+        Real denom1 = 0;
+        Real denom2 = 0;
+
+        for(std::size_t y=mask.lower_bounding_y(); y <= mask.upper_bounding_y(); ++y)
         {
-            // in a bounding rectangle, still there could be non-covered pixels.
-            if(mask(x, y)) {continue;}
+            for(std::size_t x=mask.lower_bounding_x(); x <= mask.upper_bounding_x(); ++x)
+            {
+                // in a bounding rectangle, still there could be non-covered pixels.
+                if(mask(x, y)) {continue;}
 
-            const auto l = lhs(x, y);
-            const auto r = rhs(x, y);
+                const auto l = lhs.at(x, y);
+                const auto r = rhs.at(x, y);
 
-            numer  += l * r;
-            denom1 += l * l;
-            denom2 += r * r;
+                numer  += l * r;
+                denom1 += l * l;
+                denom2 += r * r;
+            }
         }
+        return numer / (denom1 * denom2);
     }
-    return numer / (denom1 * denom2);
-}
+};
 
-template<typename Real, typename Mask>
-Real score_root_mean_squared_deviation(
-        const stage<Real>& lhs, const stage<Real>& rhs, const Mask& mask)
+
+struct score_root_mean_squared_deviation_t
 {
-    std::uint64_t N = 0;
-    Real sd = 0.0;
-
-    for(std::size_t y=mask.lower_bounding_y(); y <= mask.upper_bounding_y(); ++y)
+    template<typename Real, typename Mask>
+    Real operator()(const stage<Real>& lhs, const stage<Real>& rhs, const Mask& mask) const
     {
-        for(std::size_t x=mask.lower_bounding_x(); x <= mask.upper_bounding_x(); ++x)
+        std::uint64_t N = 0;
+        Real sd = 0.0;
+
+        for(std::size_t y=mask.lower_bounding_y(); y <= mask.upper_bounding_y(); ++y)
         {
-            // in a bounding rectangle, still there could be non-covered pixels.
-            if(mask(x, y)) {continue;}
+            for(std::size_t x=mask.lower_bounding_x(); x <= mask.upper_bounding_x(); ++x)
+            {
+                // in a bounding rectangle, still there could be non-covered pixels.
+                if(mask(x, y)) {continue;}
 
-            const auto l = lhs(x, y);
-            const auto r = rhs(x, y);
+                const auto l = lhs(x, y);
+                const auto r = rhs(x, y);
 
-            sd += (l - r) * (l - r);
-            N  += 1;
+                sd += (l - r) * (l - r);
+                N  += 1;
+            }
         }
+        return std::sqrt(sd / static_cast<Real>(N));
     }
-    return std::sqrt(sd / static_cast<Real>(N));
-}
+};
 
 } // afmize
 #endif// AFMIZE_MASK_HPP
