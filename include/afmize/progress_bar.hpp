913211ab88e21ea719af27fce617dd5cc298001b
ToruNiina <niina.toru.68u@gmail.com>
2020-11-12 00:04:30 +0900

    :sparkles: estimate the remaining time

--- include/afmize/progress_bar.hpp
+++ include/afmize/progress_bar.hpp
@@ -1,10 +1,14 @@
 #ifndef AFMIZE_PROGRESS_BAR_HPP
 #define AFMIZE_PROGRESS_BAR_HPP
-#include <cstring>
-#include <cmath>
+
 #include <array>
+#include <chrono>
+#include <sstream>
+
 #include <cassert>
+#include <cmath>
 #include <cstdio>
+#include <cstring>
 
 /* This code is originally implemented for other project named Mjolnir,       *
  * by the same author, Toru Niina (c) 2018. destributed under the MIT License.*/
@@ -25,76 +29,117 @@ class progress_bar
     static constexpr auto l2    = u8"▎"; // U+258E Left one quarter block
     static constexpr auto l1    = u8"▏"; // U+258F Left one eighth block
 
-    // \r100.0%| ... W*3 ... |\0
-    static constexpr std::size_t buffer_size = 10 + Width * 3;
-
   public:
 
-    progress_bar(std::size_t tot) : total_(tot), r_total_(1.0 / tot)
-    {
-        buffer_.fill('\0');
-        buffer_.front() = '\r';
-    }
+    explicit progress_bar(std::size_t tot)
+        : total_(tot), r_total_(1.0 / tot), start_(std::chrono::system_clock::now())
+    {}
     ~progress_bar() = default;
     progress_bar(progress_bar const&) = default;
     progress_bar(progress_bar &&)     = default;
     progress_bar& operator=(progress_bar const&) = default;
     progress_bar& operator=(progress_bar &&)     = default;
 
-    const char* format(std::size_t count)
+    std::string format(std::size_t count)
     {
-        const double ratio = (count == total_) ? 1.0 : count * this->r_total_;
-        assert(ratio <= 1.0);
+        if(count == 0){start_ = std::chrono::system_clock::now();}
+        std::ostringstream os;
 
-        char* iter = buffer_.data();
-        iter++; // the first character is always \r
-        iter += std::sprintf(iter, "%5.1f", ratio * 100.0);
-        *iter++ = '%';
-        *iter++ = '|';
+        //XXX: requires UTF-8.
+        constexpr auto full  = u8"█"; // U+2588 Full block
+        constexpr auto l7    = u8"▉"; // U+2589 Left seven eighths block
+        constexpr auto l6    = u8"▊"; // U+258A Left three quarters block
+        constexpr auto l5    = u8"▋"; // U+258B Left five eighths block
+        constexpr auto l4    = u8"▌"; // U+258C Left half block
+        constexpr auto l3    = u8"▍"; // U+258D Left three eighths block
+        constexpr auto l2    = u8"▎"; // U+258E Left one quarter block
+        constexpr auto l1    = u8"▏"; // U+258F Left one eighth block
+
+        const double ratio = (count == total_) ? 1.0 :
+            std::max(0.0, std::min(1.0, count * this->r_total_));
+
+        std::array<char, 8> buf;
+        buf.fill('\0');
+        std::snprintf(buf.data(), 8, "%5.1f%%|", ratio * 100.0);
+        os << '\r' << buf.data();
 
         const std::size_t filled = std::floor(ratio*Width);
         for(std::size_t i=0; i<filled; ++i)
         {
-            *iter++ = full[0];
-            *iter++ = full[1];
-            *iter++ = full[2];
+            os << full;
         }
         if(Width > filled)
         {
             switch(static_cast<std::size_t>(ratio * Width * 8) % 8)
             {
-                case 0:{*iter++ = ' '; break;}
-                case 1:{*iter++ = l1[0]; *iter++ = l1[1]; *iter++ = l1[2]; break;}
-                case 2:{*iter++ = l2[0]; *iter++ = l2[1]; *iter++ = l2[2]; break;}
-                case 3:{*iter++ = l3[0]; *iter++ = l3[1]; *iter++ = l3[2]; break;}
-                case 4:{*iter++ = l4[0]; *iter++ = l4[1]; *iter++ = l4[2]; break;}
-                case 5:{*iter++ = l5[0]; *iter++ = l5[1]; *iter++ = l5[2]; break;}
-                case 6:{*iter++ = l6[0]; *iter++ = l6[1]; *iter++ = l6[2]; break;}
-                case 7:{*iter++ = l7[0]; *iter++ = l7[1]; *iter++ = l7[2]; break;}
+                case 0: {os << ' '; break;}
+                case 1: {os << l1;  break;}
+                case 2: {os << l2;  break;}
+                case 3: {os << l3;  break;}
+                case 4: {os << l4;  break;}
+                case 5: {os << l5;  break;}
+                case 6: {os << l6;  break;}
+                case 7: {os << l7;  break;}
             }
-            for(std::size_t i=0; i<Width - filled - 1; ++i)
+            for(std::size_t i=1; i<Width - filled; ++i)
             {
-                *iter++ = ' ';
+                os << ' ';
             }
         }
-        *iter++ = '|';
-        *iter++ = '\0';
-        return buffer_.data();
-    }
+        os << '|';
 
-    std::size_t total() const noexcept {return total_;}
+        if(ratio == 0.0)
+        {
+            os << std::flush;
+            return os.str();
+        }
+
+        const auto current  = std::chrono::system_clock::now();
+        const auto residual = std::chrono::duration_cast<std::chrono::milliseconds>(
+            (current - start_) * (1.0 - ratio) / ratio).count() * 0.001;
+
+        buf.fill('\0');
+        if(residual < 60.0)
+        {
+            std::snprintf(buf.data(), 6, "%5.1f", residual);
+            os << buf.data() << " seconds remaining ";
+        }
+        else if(residual < 60.0 * 60.0)
+        {
+            std::snprintf(buf.data(), 6, "%5.1f", residual * 0.0167);
+            os << buf.data() << " minutes remaining ";
+        }
+        else if(residual < 60.0 * 60.0 * 24.0)
+        {
+            std::snprintf(buf.data(), 6, "%5.1f", residual * 0.0167 * 0.0167);
+            os << buf.data() << " hours remaining   ";
+        }
+        else if(residual < 60.0 * 60.0 * 24.0 * 99.0)
+        {
+            std::snprintf(buf.data(), 6, "%5.1f", residual * 0.0167 * 0.0167 * 0.0417);
+            os << buf.data() << " days remaining    ";
+        }
+        else
+        {
+            os << " over 100 days remaining";
+        }
+        return os.str();
+    }
 
     void reset_total(std::size_t t)
     {
         this->total_   = t;
         this->r_total_ = 1.0 / t;
+        this->start_   = std::chrono::system_clock::now();
     }
+
+    std::size_t total() const noexcept {return total_;}
 
   private:
 
     std::size_t total_;
     double      r_total_;
-    std::array<char, buffer_size> buffer_;
+    std::chrono::system_clock::time_point start_;
 };
 
 
